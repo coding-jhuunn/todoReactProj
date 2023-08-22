9bca53d60c53ab220b0ef9240e72fec3eabaa85a
jhun <aljhun.castillo14@gmail.com>
2023-08-22 21:47:11 +0800

    add check duplicate item

--- todoproject/src/Components/Form.js
+++ todoproject/src/Components/Form.js
@@ -5,26 +5,37 @@ import style from './style.module.css';
 
 const Form = ({itemTodo,setItemTodo,todoList,setTodoList,error,setError})=>{
     
+    const CheckIfExsts = (arr,value) =>{
+        const found = arr.some(el => el.name === value);
+        if(found){
+            return true
+        }
+    }
+ 
+
+    
   
     const handleChangeInput = (e)=>{
             setItemTodo(e.target.value);
     }
     const handleAddTodo = (e)=>{
+        
         e.preventDefault();
-
-
         if(itemTodo.length===0){
             setError(true);
             console.log("none")
         }
         else if (itemTodo.length>0){
-            setTodoList([...todoList,{name:itemTodo,id:shortid.generate()}]);
-            setItemTodo("");
-            setError(false);
-        }
-
-        
-        
+            if(CheckIfExsts(todoList, itemTodo)){
+                console.log("exists");
+            }
+            else{
+                setTodoList([...todoList,{name:itemTodo,id:shortid.generate()}]);
+                setItemTodo("");
+                setError(false);
+                console.log("not exists")
+            }
+        }  
     }
     
     return (
@@ -37,8 +48,6 @@ const Form = ({itemTodo,setItemTodo,todoList,setTodoList,error,setError})=>{
                 <div>
                     <button className={style.formbtn} >ADD</button>
                 </div>
-              
-            
             </form>
         </div>
     )
