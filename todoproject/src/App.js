6bb02b63970cdad0a05c821e46c7c883057c9f53
jhun <aljhun.castillo14@gmail.com>
2023-08-24 16:03:31 +0800

    local storage add

--- todoproject/src/App.js
+++ todoproject/src/App.js
@@ -3,12 +3,24 @@ import Form from "./Components/Form";
 import Header from "./Components/Header";
 import RenderList from "./Components/RenderList";
 import "./main.css"
-import {useState} from 'react';
+import { useEffect, useState } from "react";
 function App() {
   const [itemTodo,setItemTodo] = useState("");
-  const [todoList,setTodoList] = useState([]);
+  const [todoList,setTodoList] = useState(()=>{
+    const savedTodoList = localStorage.getItem('todolist');
+    if(savedTodoList){
+      return JSON.parse(savedTodoList);
+    }
+    else{
+      return [];
+    }
+  });
+    useEffect(()=>{
+      localStorage.setItem('todolist',JSON.stringify(todoList));
+    },[todoList]);
   const [error,setError] = useState(false);
   const [duplicate,setDuplicate] = useState(false);
+
   return (
     <div className="App">
       <Header></Header>
